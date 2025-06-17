7d8ea514adbf0b3789a724a4201f8bc8294e408d
sporlichka <tvooybatya@gmail.com>
2025-06-17 15:07:37 +0500

    eslint

--- video-front/eslint.config.js
+++ video-front/eslint.config.js
@@ -7,11 +7,22 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      // Uncomment for stricter rules:
+      // ...tseslint.configs.strictTypeChecked,
+      // Uncomment for stylistic rules:
+      // ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
