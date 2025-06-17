95ad500421e11571864295e61a03cff8aae1f5af
sporlichka <tvooybatya@gmail.com>
2025-06-17 15:14:58 +0500

    cors added

--- video-back/app/main.py
+++ video-back/app/main.py
@@ -21,7 +21,8 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://pet-care-testing-functioning.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
